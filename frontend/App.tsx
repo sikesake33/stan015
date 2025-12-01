1e011179b17f4f379c7a35d144469464f22da035
sikesake33 <bnedeljkovic015@gmail.com>
2025-12-01 14:57:44 +0100

    Initialize App component with Home screen

---
+++ frontend/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { SafeAreaView } from "react-native";
+import Home from "./src/screens/Home";
+
+export default function App() {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <Home />
+    </SafeAreaView>
+  );
+}
